8e82b987413036add60685ebcd6cd317cebe6a71
Djkde01 <sergio.estrella@utp.edu.co>
2022-06-07 23:30:39 -0500

    feat(created tables and connected db)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[macro_use]
+extern crate diesel;
+
+pub mod models;
+pub mod schema;
+
+use dotenv::dotenv;
+use std::env;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let connection = PgConnection::establish(&db_url).expect("Error connecting to database");
+
+    use self::models::Post;
+
+    use self::schema::posts::dsl::*;
+
+    let results = posts
+        .load::<Post>(&connection)
+        .expect("Error loading posts")
+        .iter()
+        .map(|post| {
+            println!("{}", post.title);
+        });
 }
