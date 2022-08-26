82dacb85399fc7c2ddfc45d6c1bc3b805873e5fc
Djkde01 <sergio.estrella@utp.edu.co>
2022-08-25 23:08:36 -0500

    feat(Created endpoints)

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use self::models::Post;
+use self::models::{NewPost, NewPostHandler, Post};
 use self::schema::posts;
 use self::schema::posts::dsl::*;
 
@@ -26,6 +26,18 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder {
     let conn = pool.get().expect("Error connecting db");
 
     match web::block(move || posts.load::<Post>(&conn)).await {
+        Ok(data) => HttpResponse::Ok().body(format!("{:?}", data)),
+        Err(err) => HttpResponse::Ok().body("Error receiving data"),
+    }
+}
+
+#[post("/new_post")]
+async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let conn = pool.get().expect("Error connecting db");
+
+    println!("{:?}", item);
+
+    match web::block(move || Post::create_post(&conn, &item)).await {
         Ok(data) => HttpResponse::Ok().body(format!("{:?}", data)),
         Err(err) => HttpResponse::Ok().body("Error receiving data"),
     }
@@ -44,6 +56,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .service(index)
+            .service(new_post)
             .app_data(web::Data::new(pool.clone()))
     })
     .bind(("0.0.0.0", 9900))
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 #[derive(Queryable)]
 pub struct SimplifiedPost {
     pub title: String,
@@ -12,7 +14,16 @@ pub struct Post {
     pub body: String,
 }
 
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
+}
+
 use super::schema::posts;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
 
 #[derive(Insertable)]
 #[table_name = "posts"]
@@ -20,4 +31,26 @@ pub struct NewPost<'a> {
     pub title: &'a str,
     pub slug: &'a str,
     pub body: &'a str,
+}
+
+impl Post {
+    pub fn slugify(title: String) -> String {
+        return title.replace(" ", "-").to_lowercase();
+    }
+    pub fn create_post<'a>(
+        conn: &PgConnection,
+        post: &NewPostHandler,
+    ) -> Result<Post, diesel::result::Error> {
+        let slug = Post::slugify(post.title.clone());
+
+        let new_post = NewPost {
+            title: &post.title,
+            slug: &slug,
+            body: &post.body,
+        };
+
+        diesel::insert_into(posts::table)
+            .values(new_post)
+            .get_result::<Post>(conn)
+    }
 }
